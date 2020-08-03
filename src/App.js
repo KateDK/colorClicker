dd50476a736b3eb8e8ea37697c5f9fa4a1e8c352
Kate Dubitski <kate.dubitski@gmail.com>
2020-08-03 17:45:51 -0400

    create and render ColorWrapper component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ColorWrapper from './ColorWrapper';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <ColorWrapper/>
     </div>
   );
 }
---
+++ src/ColorWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ColorWrapper extends React.Component {
+  render(){
+    return (
+      <div className="ColorWrapper">
+        Im a div!
+      </div>
+    );
+  }
+}
+
+export default ColorWrapper;

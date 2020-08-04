8e8a9327da591eeac885cff919da3c5ebc94066c
Kate Dubitski <kate.dubitski@gmail.com>
2020-08-04 15:14:18 -0400

    use new handler in Button component

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({color="Silver", message="click here",onClick=()=>console.log("Click!")}) => {
-  // const {onClick,color}=props;
+
   return(
-  <button style={{backgroundColor:color}} onClick={onClick}>
+  <button style={{backgroundColor:color}} onClick={()=>onClick(color)}>
     {message}
   </button>
 );

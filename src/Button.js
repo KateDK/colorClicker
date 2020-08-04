a27cecf24ef0246566960fa3b20cea74f08da1f1
Kate Dubitski <kate.dubitski@gmail.com>
2020-08-04 16:14:26 -0400

    change handler invocation in button

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({color="Silver", message="click here",onClick=()=>console.log("Click!")}) => {
-
+const handleClick = () => onClick(color);
   return(
-  <button style={{backgroundColor:color}} onClick={()=>onClick(color)}>
+  <button style={{backgroundColor:color}} onClick={handleClick}>
     {message}
   </button>
 );
